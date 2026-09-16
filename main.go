f2bd5c0807c732eea930a4c8521e9b822f172967
TTa-riTM <azle65200@gmail.com>
2026-09-16 18:25:30 +0300

    Добавил https и перевод

--- main.go
+++ main.go
@@ -4,34 +4,73 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	// Сообщаем браузеру, что передаем HTML-разметку
+var dict = map[string]map[string]string{
+	"ru": {
+		"switch": "en", "switch_text": "English",
+		"name": "Имя (string)", "age": "Возраст (int)", "role": "Роль (enum)",
+		"agree": "Согласие (bool)", "submit": "Отправить", "result": "Результат POST-запроса:",
+	},
+	"en": {
+		"switch": "ru", "switch_text": "Русский",
+		"name": "Name (string)", "age": "Age (int)", "role": "Role (enum)",
+		"agree": "Agree (bool)", "submit": "Submit", "result": "POST Result:",
+	},
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	name := r.URL.Query().Get("name")
+	lang := r.URL.Query().Get("lang")
+	if lang != "en" && lang != "ru" {
+		lang = "ru"
+	}
+	t := dict[lang]
 
-	fmt.Fprintf(w, `
-		<form method="GET">
-			<input type="text" name="name" placeholder="Enter your name" value="%s">
-			<button type="submit">Submit</button>
-		</form>
-	`, name)
+	getName := r.URL.Query().Get("name")
 
-	if name != "" {
-		fmt.Fprintf(w, "<h3>Hello, %s!</h3>", name)
+	fmt.Fprintf(w, "<p><a href=\"/?lang=%s&name=%s\">%s</a></p>", t["switch"], getName, t["switch_text"])
+
+	if getName != "" {
+		fmt.Fprintf(w, "<p>GET name: <b>%s</b></p>", getName)
+	}
+
+	fmt.Fprintf(w, "<form method=\"POST\" action=\"/?lang=%s\">"+
+		"<p>%s: <input type=\"text\" name=\"name\" value=\"%s\" required></p>"+
+		"<p>%s: <input type=\"number\" name=\"age\" required></p>"+
+		"<p>%s: "+
+		"<select name=\"role\">"+
+		"<option value=\"Developer\">Developer</option>"+
+		"<option value=\"QA\">QA</option>"+
+		"</select>"+
+		"</p>"+
+		"<p><label><input type=\"checkbox\" name=\"agree\" value=\"true\"> %s</label></p>"+
+		"<button type=\"submit\">%s</button>"+
+		"</form>",
+		lang, t["name"], getName, t["age"], t["role"], t["agree"], t["submit"])
+
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+
+		name := r.FormValue("name")
+		age, _ := strconv.Atoi(r.FormValue("age"))
+		role := r.FormValue("role")
+		agree := r.FormValue("agree") == "true"
+
+		fmt.Fprintf(w, "<hr><h3>%s</h3>", t["result"])
+		fmt.Fprintf(w, "<p>name: %s | age: %d | role: %s | agree: %t</p>", name, age, role, agree)
 	}
 }
 
 func main() {
-	port := flag.String("port", "8080", "порт для запуска сервера")
+	port := flag.String("port", "8443", "порт сервера")
 	flag.Parse()
 
-	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/", handler)
 
 	addr := ":" + *port
-	fmt.Printf("Сервер запущен. Откройте в браузере http://localhost%s/form\n", addr)
-
-	http.ListenAndServe(addr, nil)
+	fmt.Printf("Сервер запущен: https://localhost%s\n", addr)
+	http.ListenAndServeTLS(addr, "server.crt", "server.key", nil)
 }
